d836f9a44261b10b4b56ac9e8006ccdca42d818b
Uyen Luu <uyen.luu@enlabsoftware.com>
2021-12-31 13:27:25 +0700

    Update api key

--- lib/src/core/utils/constants.dart
+++ lib/src/core/utils/constants.dart
@@ -3,7 +3,7 @@ const String kMaterialAppTitle = 'Flutter Clean Architecture';
 // API
 const String kBaseUrl = 'https://newsapi.org/v2';
 
-const String kApiKey = 'ff957763c54c44d8b00e5e082bc76cb0';
+const String kApiKey = '054ac37a490e4f158f5c1055900107e3';
 
 // Database
 const String kArticlesTableName = 'articles_table';
